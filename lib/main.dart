53acb4c0b27fe36dfaf3af372122696947aba760
FarahIzzah21 <farahizzah@graduate.utm.my>
2023-12-05 10:21:35 +0800

    changed main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,79 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Homestay List',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Homestay List'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: HomestayRow(
+                  homestays: [
+                    {'name': 'Angsana House', 'color': Colors.red},
+                    {'name': 'Apsara Villa', 'color': Colors.red},
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: HomestayRow(
+                  homestays: [
+                    {'name': 'Chenang Stay', 'color': Colors.blue},
+                    {'name': 'Indah Home', 'color': Colors.blue},
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: HomestayRow(
+                  homestays: [
+                    {'name': 'Dini Pavi', 'color': Colors.green},
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      home: HomestayList(),
     );
   }
 }
 
-class HomestayList extends StatelessWidget {
-  // List of homestays
-  final List<String> homestays = [
-    'Angsana House',
-    'Apsara Villa',
-    'Chenang Stay',
-    'Indah Home',
-    'Dini Pavi',
-  ];
+class HomestayRow extends StatelessWidget {
+  final List<Map<String, dynamic>> homestays;
+
+  HomestayRow({required this.homestays});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Homestay List'),
-      ),
-      body: ListView.builder(
-        itemCount: homestays.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(homestays[index]),
-          );
-        },
-      ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: homestays.map((homestay) {
+        return Container(
+          margin: const EdgeInsets.all(8.0),
+          width: 200,
+          height: 100,
+          color: homestay['color'],
+          child: Center(
+            child: Text(
+              homestay['name'],
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+        );
+      }).toList(),
     );
   }
 }
