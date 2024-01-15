abafdbc521fea5fcb5124d4f807dcc064fcc0ed5
nurdinifatini1 <147517123+nurdinifatini1@users.noreply.github.com>
2024-01-16 00:50:17 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
+import 'package:annahomestay/models/availabilityscreen.dart';
+import 'package:annahomestay/models/homestayfilter.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart';
 import 'listscreen.dart';
-import 'bookingscreen.dart';
+import 'bookingscreenold.dart';
 import 'confirmationscreen.dart';
-import 'package:razorpay_flutter/razorpay_flutter.dart';
+import 'repository/booking_repository.dart';
+import 'models/bookingscreen.dart';
+import 'models/descriptionscreen.dart';
+import 'package:get/get.dart';
+//import 'package:stripe_payment/stripe_payment.dart';
+//import 'package:razorpay_flutter/razorpay_flutter.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,13 +33,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Homestay App',
-      initialRoute: '/list',
+    //  home: HomestayFilterPage(),
+      debugShowCheckedModeBanner: false,
+     initialRoute: '/list',
       routes: {
         '/list': (context) => ListScreen(),
+        '/filter':(context) => HomestayFilterPage(),
         '/booking': (context) => BookingScreen(),
+        '/availability': (context) => AvailabilityScreen(),
         '/confirmation': (context) => ConfirmationScreen(),
-        
-      },
+        '/description': (context) => DescriptionScreen(),
+      }, 
     );
   }
 }
